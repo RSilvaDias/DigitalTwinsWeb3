e1e72acb266e0f4eae944626a3ab148b634fc748
Renato S Dias <39096504+RSilvaDias@users.noreply.github.com>
2023-10-17 12:39:18 -0300

    Add files via upload

---
+++ WearableClient.py
@@ -0,0 +1,55 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import json
+import time
+import random
+
+# MQTT config (clientID must be unique within the AWS account)
+clientID = "Wearable1"
+endpoint = "a14181np570969-ats.iot.us-east-1.amazonaws.com" #Use the endpoint from the settings page in the IoT console
+port = 8883
+topic = "wearable/temperature"
+
+# Init MQTT client
+mqttc = AWSIoTMQTTClient(clientID)
+mqttc.configureEndpoint(endpoint,port)
+mqttc.configureCredentials("certificates/AmazonRootCA1.pem","certificates/wearable-private.pem.key","certificates/wearable-certificate.pem.crt")
+
+# Send message to the iot topic
+def send_data(message):
+    mqttc.publish(topic, json.dumps(message), 0)
+    print("Message Published")
+
+# Loop until terminated
+def loop():
+     
+     while(True):
+          try:
+               temperature = random.uniform(36.5, 39.0)
+               bpm = random.randint(70,100)
+               print("Temp: {:.1f} C    BPM: {} ".format(temperature, bpm)) 
+               
+               message = {
+                         'temperature': temperature,
+                         'bpm': bpm
+                         }
+                    
+               # Send data to topic
+               send_data(message)
+
+               time.sleep(5)
+          except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+               print(error.args[0])
+
+# Main
+if __name__ == '__main__':
+    print("Starting program...")
+    try:
+        # Connect
+        mqttc.connect()
+        print("Connect OK!")
+
+        # Main loop called
+        loop()
+    except KeyboardInterrupt:
+        mqttc.disconnect()
+        exit()
